d3348b4c15e8252aa3aa845e25f0b032c39ca5ef
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-08 14:33:54 +0600

    added

---
+++ exploring-javaScript/index.js
@@ -0,0 +1,31 @@
+/**
+ * core concept in javaScript
+ * -----------------------------
+ * 1. Variable
+ * 2. Function
+ * 3. Condition
+ * 4. Loop
+ *
+ */
+/**
+ * Data types in javaScript
+ * ------------------------------
+ * # There are 2 types or data
+ *
+ * 1. Primitive = Number, String, Boolean, Undefined, Null, Symbol
+ * 2. Reference = Object, Array
+ */
+
+/**
+ * Task
+ * -----------
+ * Array method
+ * String method
+ * Date method
+ * Dom method
+ * Dom properties
+ * Event
+ */
+
+const userName = "Sk";
+console.log(userName);
